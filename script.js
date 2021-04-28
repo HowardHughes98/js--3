fa5655237efeb5b1793ffee71bd3011ef8aacce9
HowardHughes98 <82488909+HowardHughes98@users.noreply.github.com>
2021-04-28 10:07:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+var a = +prompt('введите число');
+var b = +prompt('введите второе число');
+var c = +prompt('введите третье число');
+ if ( a < b <c ) {
+     alert(b);
+ } else if ( b < a < c) {
+     alert( a );
+ } else if ( b < c < a) {
+     alert( c );
+ }
